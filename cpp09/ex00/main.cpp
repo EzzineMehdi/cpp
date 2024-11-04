29a03c1b431bf12fc7dc3de2b70fa3e7c8d8918e
Mehdi Ezzine <mezzine@e1r8p11.1337.ma>
2024-11-04 23:09:46 +0100

    | parsed

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,5 +1,16 @@
 #include "BitcoinExchange.hpp"
 
+bool	isFloat(const std::string &str)
+{
+	std::map<size_t, std::string>	strs = split(str, ".");
+
+	if (countChar(str, '.') != 1 || strs.size() != 2)
+		return false;
+	if (!isDigit(strs[0]) || !isDigit(strs[1]))
+		return false;
+	return true;
+}
+
 void	parseInput(const std::string &str, const BitcoinExchange &b)
 {
 	double	data[2];
@@ -10,6 +21,8 @@ void	parseInput(const std::string &str, const BitcoinExchange &b)
 	
 	Date date(strs[0]);
 		data[0] = b.getPrice(date);
+	if (!isDigit(strs[1]) && !isFloat(strs[1]))
+		throw std::runtime_error("Error: bad input => " + str);
 	data[1] = atof(strs[1].c_str());
 	if (data[1] < 0 || data[1] > 1000)
 		throw data[1] < 0 ? std::runtime_error("Error: not a positive number.")
